881a388eb86a9ba57258ea2c870654be856283ef
gpatel314 <gpatel@iastate.edu>
2025-09-17 20:07:31 -0500

    Pusing basic servo code

---
+++ Code/ServoCode/sketch_apr23a/sketch_apr23a.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+Servo servo;
+int pwm = 11;
+int pos = 0;
+
+void setup() {
+pinMode(pwm, OUTPUT);
+servo.attach(11);
+}
+//next steps:
+//make it go 90 degress stop and back to start point
+//make it delay for a certain amount of hours
+void loop() {
+  for(pos=0;pos<=180;pos+=1){
+    servo.write(pos);
+    delay(15);
+  }
+  for(pos=180;pos>=0;pos-=1){
+    servo.write(pos);
+    delay(15);
+  }
+}
