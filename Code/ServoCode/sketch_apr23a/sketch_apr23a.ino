4e14d3bed859806bcbae215bc649ab4a4cddb94f
itsrohita <rohitashomebase@gmail.com>
2025-11-18 17:49:01 -0600

    Updated code file. Should be functional now :) hi from Lavanya

--- Code/ServoCode/sketch_apr23a/sketch_apr23a.ino
+++ Code/ServoCode/sketch_apr23a/sketch_apr23a.ino
@@ -1,22 +1,24 @@
-#include <Servo.h>
-Servo servo;
-int pwm = 11;
-int pos = 0;
+// UNO SPECIFIC CODE: #include <Servo.h>
+#include <ESP32Servo.h> // ESP32 SPECIFIC CODE
+
+Servo myServo;
+// UNO CODE: int pwm = 11;
 
 void setup() {
-pinMode(pwm, OUTPUT);
-servo.attach(11);
+  // UNO SPECIFIC CODE: pinMode(pwm, OUTPUT)
+  // UNO SPECIFIC CODE: myServo.attach(11);
+  
+  myServo.attach(15); // ESP32 SPECIFIC CODE
 }
-//next steps:
-//make it go 90 degress stop and back to start point
-//make it delay for a certain amount of hours
+
+// for modifications: tweak delay(# of milliseconds)
 void loop() {
-  for(pos=0;pos<=180;pos+=1){
-    servo.write(pos);
-    delay(15);
-  }
-  for(pos=180;pos>=0;pos-=1){
-    servo.write(pos);
-    delay(15);
-  }
+  myServo.writeMicroseconds(1700); // makes it go forward 
+  delay(7000);
+
+  myServo.writeMicroseconds(1500); // makes it stop
+  delay(1000);
+
+  myServo.writeMicroseconds(1300); // makes it go backward
+  delay(7000);
 }
